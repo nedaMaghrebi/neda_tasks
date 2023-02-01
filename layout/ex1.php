d90c646158e5731da9c47d7afa0b406f921ebf2f
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-01 11:30:39 +0200

    aa

--- layout/ex1.php
+++ layout/ex1.php
@@ -11,4 +11,37 @@ echo "Hello World! My name is \"neda maghrabi\" ";
 ?>
 <h3> Current Date</h3>
 <?php echo date("m . d . y") ?>
+
+<h3> 3.4 Variable in Php</h3>
+<?php 
+$titel1="PHP is interesting";
+echo " <h1>" . $title1. " <h1>";
+?>
+
+<h3> 3.5 Table & Variables</h3>
+<?php 
+$g1=5;
+$g2=4;
+$g3=5;
+echo "
+<table>
+<tr>
+<th> S.N </th> <th> Name </th> <th> Grade </th>
+</tr>
+<tr>
+<td> 1 </td> <td> Pekka </td> <td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td> <td> Johanna </td> <td> $g2 </td>
+</tr>
+<tr>
+<td> 3 </td> <td> John </td> <td> $g3 </td>
+</tr>
+<table>
+";
+?>
+
+<h3> 4 Screenshot </h3>
+<img src="images/1.jpg" alt="first shot">
 <?php include "footer.php"?>
+
