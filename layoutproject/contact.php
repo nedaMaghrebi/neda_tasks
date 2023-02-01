7400a68d917f6ce57b34b02d61df4ae8a71b0962
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-01 19:48:29 +0200

    I added the CSS codes

---
+++ layoutproject/contact.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Contact:Twinkling Stars Kindergarten" ;
+include "headerproject.php"?>
+
+<img src="https://s3-alpha-sig.figma.com/img/c526/a182/31a333572c962828c218c2ae894e102e?Expires=1675641600&Signature=AOIz0byh7tn7bepNARUmpliBCVcjgl8BfGLVLsjloiUTxO0hiZLXtEdLoIWI3-5FTo~WV3Fm~j~-YyQmAGTyP47fLzK26631N2S008dNBj12waclhvwjlaXFTNMHsei3rCN0DNrx7BS-bc6ya~H4Jgi-3ERNXpIBU57~YWhdKu8Q0qBzyd3b25VPP9ORUyU0Vz05U9ruNSuUsuQnaQ2DvNc~yysA1V4bcTxLPFUmzQwJxPfmmhZuyCSUUZQ7FOoMtc7khAJYLrk0jW35kRo2JfVwLwWVcUES8a6wmghM5Z~JJiRQm0CDGcKqQM3j0amgwbMuu9RV1XRnKRiVrvAz5w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="contact" class="rounded" >
+            </div>
+            <div class="column" style="text-align:left;">
+            <ul style="list-style-type:none;">
+                <div class="contact1" style="font-size: 40px; font-weight: bolder;">
+                <li>Contact Us <br></li>
+                </div>
+
+                <div class="keepintouch" style="font-size: 30px;">
+                <li>   Keep In Touch</li><br>
+                </div>
+                <li>Twinkling Stars Kindergarten  <br>Visamäentie 23,
+                <br>Hämeenlinna 13100</li>
+                <li><i class="material-icons" style="font-size:60px;color:blue;">phone</i>415-218-4791</li>
+                <li><i class="material-icons" style="font-size:60px;color:blue;">email</i><a href="mailto:info@twinklingstars.com">info@twinklingstars.com</a></a></li>
+                <li><i class="material-icons" style="font-size:60px;color:blue;">facebook</i><a href="https://www.facebook.com/profile.php?id=100089799731637">Like Us On Facebook</a></li>
+            </ul>
+
+            <?php include "footerproject.php"?>
---
+++ layoutproject/enroll.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Enroll:Twinkling Stars Kindergarten" ;
+include "headerproject.php"?>  
+
+<img class="header-image" src="https://i.pinimg.com/originals/64/07/f8/6407f8b72e8c1afa198ddd37b81baef6.jpg " alt="background image">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+
+    <div class="row">
+        <div class="col-md-12">
+        <h2><b><i>ADMISSION OPEN FOR 2023</i></b></h2>
+        </div>
+    </div>  
+    <div class="row">
+        <div class="col-md-12">
+        <h3><b> Download our enrollment form and email to</b></h3>
+        <h4><b> info@twinklingstars.com</b></h4>
+        <a class="btn btn-danger" role="button" href="enrollmentfoam.pdf">Enrollment form</a>
+
+
+
+<?php include "footerproject.php"?>
