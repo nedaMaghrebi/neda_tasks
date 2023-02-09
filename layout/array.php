5a31927e84540672512ef562fa89a660ca97e5e8
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-09 11:32:34 +0200

    3 and 4 are done

--- layout/array.php
+++ layout/array.php
@@ -13,15 +13,36 @@ for ($i = 0; $i < count($courses); $i++)
 
 <h3>2.  The unset() function is used to remove element from the array.</h3>
 <?php
-$courses=array("PHP ", "HTML ", "JavaScript ", "CMS ", "Project ");
-unset($courses[1]);
-echo implode(',' , $courses);
+$courses1=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+unset($courses1[1]);
+echo implode(',' , $courses1);
 ?>
 
 <h3>3. Sort the following array .</h3>
 <?php
-$courses=array("PHP ", "HTML ", "JavaScript ", "CMS ", "Project ");
-sort($courses);
-echo ""
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+sort($courses3);
+echo "Ascending order by value:". implode(',' , $courses3)."<br>";
+ksort($courses3);
+echo "Ascending order  by Key:". implode(',' , $courses3)."<br>";
+arsort($courses3);
+echo "Decending order by value:". implode(',' , $courses3)."<br>";
+krsort($courses3);
+echo "Decending order by Key:". implode(',' , $courses3)."<br>";
+?>
+
+<h3>4. Change the following array's all values to upper case. .</h3>
+<?php 
+ $courses4=array("php", "html", "javascript", "cms", "project");
+ for($i=0 ; $i<count($courses4) ; $i++){
+ $courses4[$i] = strtoupper($courses4[$i]);
+}
+echo implode(',' ,$courses4);
+?>
+
+<h3>5. List all your favorite colors and their hexadecimal equivalents. (associative arrays).</h3>
+<?php 
+
+
 
 ?>
