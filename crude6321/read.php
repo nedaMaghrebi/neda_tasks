b4cd70dc23efe96e24a9e90278bd1b2c213fcf54
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-14 13:15:13 +0200

    delet

---
+++ crude6321/read.php
@@ -0,0 +1,47 @@
+<?php
+
+$title = "Read data from database";
+
+include "../layout/header.php";
+include 'db.php';
+$sql ="select * from studentinfo";
+$result = $conn->query($sql);
+
+
+if($result->num_rows > 0){
+    echo" <table class = 'table'>
+        <tr>
+        <th>    Id         </th> 
+        <th>    First Name </th> 
+        <th>    Last Name  </th> 
+        <th>    City       </th> 
+        <th>    Groupid    </th></tr>";
+
+
+        while($row = $result -> fetch_assoc()) {
+            echo "
+            <tr>
+            <td><a href='updatesingle.php?id=$row[id]'>$row[id] </a>     </td>
+            <td> $row[fname]     </td>
+            <td> $row[lname]     </td>
+            <td> $row[city]      </td>
+            <td> $row[groupid]   </td>
+            </tr>
+            ";
+    }
+    echo "</table>";
+}
+else
+{
+    echo "No result";
+}
+
+$conn-> close();
+
+?>
+
+
+<?php ?>
+
+<?php include "../layout/footer.php" ?>
+        
