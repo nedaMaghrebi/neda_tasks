e1e00eb53f31edbef16a106eb9ddd6d2fa7b1bd2
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-22 13:13:35 +0200

    add jsinclass.php

---
+++ layout/jsinclass.php
@@ -0,0 +1,8 @@
+<?php 
+$title="JavaScript Events and form validation";
+
+include "header.php"
+?>  
+<h2> A simple button to display date</h2>
+
+<button onclick="this.innerHTML= Date()"> Click to know date </button>
