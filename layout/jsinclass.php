c273c96e309dd2a1f1848e72cf5198bd886e7611
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-22 13:17:53 +0200

    add code with getelementbyid

--- layout/jsinclass.php
+++ layout/jsinclass.php
@@ -4,5 +4,9 @@ $title="JavaScript Events and form validation";
 include "header.php"
 ?>  
 <h2> A simple button to display date</h2>
-
 <button onclick="this.innerHTML= Date()"> Click to know date </button>
+
+
+<h2> Make a button and a place holder as paragraph for the date </h2>
+<button onclick="getElementById('p1').innerHTML= Date()"> Click to know date </button>
+<p id="p1"> </p>
